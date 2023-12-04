52e5ab8d33adbfa54461603b15e98ee93a91d89c
Ruben <adityaprasad7123@gmail.com>
2023-12-03 23:36:00 -0400

    Made function to wrte to tables to the database

--- server/database.py
+++ server/database.py
@@ -25,11 +25,12 @@ class Database:
         """Code to add tables to database"""
 
         # Dictionary for tables and the query used to create them
+        # pylint: disable=line-too-long
         tables = {
-            "vehicle_table": "CREATE TABLE IF NOT EXISTS Vehicle (VID INT(6) PRIMARY KEY, District VARCHAR(20), Section VARCHAR(40), `Insurance Exp` DATE, `Fitness Exp` DATE, `License Exp` DATE, `Vehicle Num` INT(9), Type VARCHAR(15), `Fuel Type` VARCHAR(15), `Unladen Weight` INT(6), Model VARCHAR(50), Make VARCHAR(20), `Engine ID` VARCHAR(30), `Chassis ID` VARCHAR(30));",
-            "driver_table": "CREATE TABLE IF NOT EXISTS Driver (DID INT(6) PRIMARY KEY, `Phone Number` INT(10), `First Name` VARCHAR(20), `Last Name` VARCHAR(20), Address VARCHAR(20), Department VARCHAR(30));",
-            "repair_table": "CREATE TABLE IF NOT EXISTS Repair (RID INT(6) PRIMARY KEY, Price DECIMAL(7,2), Descp TEXT, `Repair Type` TEXT);",
-            "mechanic_table": "CREATE TABLE IF NOT EXISTS Mechanic (MID INT(6) PRIMARY KEY, Name VARCHAR(50), `Phone Number` INT(10), Address VARCHAR(40));",
+            "vehicle_table": "CREATE TABLE IF NOT EXISTS vehicle (VID INT(6) PRIMARY KEY NOT NULL AUTO_INCREMENT, district VARCHAR(20), section VARCHAR(40), insuranceExp DATE, fitnessExp DATE, licenseExp DATE, vehicleNum INT(9), type VARCHAR(15), fuelType VARCHAR(15), unladenWeight INT(6), model VARCHAR(50), make VARCHAR(20), engineId VARCHAR(30), chassisId VARCHAR(30));",
+            "driver_table": "CREATE TABLE IF NOT EXISTS driver (DID INT(6) PRIMARY KEY NOT NULL AUTO_INCREMENT, phoneNumber INT(10), firstName VARCHAR(20), lastName VARCHAR(20), address VARCHAR(125), department VARCHAR(30));",
+            "repair_table": "CREATE TABLE IF NOT EXISTS repair (RID INT(6) PRIMARY KEY NOT NULL AUTO_INCREMENT, price DECIMAL(7,2), description TEXT, repairText TEXT);",
+            "mechanic_table": "CREATE TABLE IF NOT EXISTS mechanic (MID INT(6) PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(50), phoneNumber INT(10), address VARCHAR(125));",
         }
 
         print("Creating Tables")
@@ -42,6 +43,77 @@ class Database:
                 print(f"Error executing SQL for table {table_name}: {e}")
                 self.connection.rollback()
 
+    def write_vehicle(self, vehicle):
+        """Code to add a vehicle to the vehicle table"""
+        try:
+            self.cursor.execute(
+                "INSERT INTO driver_table (district, section, insuranceExp, fitnessExp, licenseExp, type, fuelType, unladenWeight, model, make, engineId, chassisId) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                (
+                    vehicle.district,
+                    vehicle.section,
+                    vehicle.insuranceExp,
+                    vehicle.fitnessExp,
+                    vehicle.licenseExp,
+                    vehicle.type,
+                    vehicle.fuelType,
+                    vehicle.unladenWeight,
+                    vehicle.model,
+                    vehicle.make,
+                    vehicle.engineId,
+                    vehicle.chassisId,
+                ),
+            )
+
+        except mysql.connector.Error as e:
+            print(f"Error executing SQL for table {vehicle.model}: {e}")
+            self.connection.rollback()
+
+    def write_driver(self, driver):
+        """Code to add a driver to the driver table"""
+        try:
+            self.cursor.execute(
+                "INSERT INTO driver_table (firstName, lastName, address, phoneNumber, department) VALUES (%s, %s, %s, %s, %s)",
+                (
+                    driver.firstName,
+                    driver.lastName,
+                    driver.address,
+                    driver.phoneNumber,
+                    driver.department,
+                ),
+            )
+        except mysql.connector.Error as e:
+            print(f"Error executing SQL for table {driver.firstName}: {e}")
+            self.connection.rollback()
+
+    def write_repair(self, repair):
+        """Code to add a repair to the repair table"""
+        try:
+            self.cursor.execute(
+                "INSERT INTO repair_table (price, description, repairText) VALUES (%s, %s, %s)",
+                (
+                    repair.price,
+                    repair.description,
+                    repair.repairText,
+                ),
+            )
+        except mysql.connector.Error as e:
+            print(f"Error executing SQL for table {repair.description}: {e}")
+            self.connection.rollback()
+
+    def write_mechanic(self, mechanic):
+        """Code to add a mechanic to the mechanic table"""
+        try:
+            self.cursor.execute(
+                "INSERT INTO mechanic_table (name, phoneNumber, address) VALUES (%s, %s, %s)",
+                (
+                    mechanic.name,
+                    mechanic.phoneNumber,
+                    mechanic.address,
+                ),
+            )
+        except mysql.connector.Error as e:
+            print(f"Error executing SQL for table {mechanic.name}: {e}")
+            self.connection.rollback()
 
 if __name__ == "__main__":
     db = Database()
