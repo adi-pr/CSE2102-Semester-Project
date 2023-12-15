6c88b47bfae8763d03af7151dd66da4270643e80
Ruben <adityaprasad7123@gmail.com>
2023-12-15 13:48:04 -0400

    Added Add Driver Tab

---
+++ client/components/AddDriver.py
@@ -0,0 +1,46 @@
+# pylint:  disable=invalid-name
+""" Module providing functions to interact with view """
+from tkinter import ttk
+
+
+class AddDriverTab:
+    # pylint:  disable=too-few-public-methods
+    """Class representing add vehicle tab"""
+
+    def __init__(self, tab_control, controller):
+        self.tab = ttk.Frame(tab_control)
+        tab_control.add(self.tab, text="Add Driver")
+        self.controller = controller
+
+        self.labels = {
+            "first_name": "First Name:",
+            "last_name": "Last Name:",
+            "phone_number": "Phone Number:",
+            "address": "Address",
+            "department": "Department",
+        }
+
+        self.entries = [ttk.Entry(self.tab) for _ in self.labels]
+
+        # pylint:  disable=unused-variable
+        for i, (key, label_text) in enumerate(self.labels.items()):
+            label = ttk.Label(self.tab, text=label_text, anchor="e")
+            label.grid(row=i, column=0, padx=15, pady=15)
+            self.entries[i].grid(row=i, column=1, padx=15, pady=15)
+
+        # Create and place the submit button
+        button_commit = ttk.Button(self.tab, text="Add Book", command=self.add_driver)
+        button_commit.grid(
+            row=len(self.labels), column=0, columnspan=2, padx=15, pady=15
+        )
+
+    def add_driver(self):
+        """Function to extract form data and commit to database"""
+        form_data = {}
+
+        for key, entry in zip(self.labels.keys(), self.entries):
+            form_data[key] = entry.get()
+
+        print(form_data)
+
+        self.controller.write_driver_to_database(form_data)
